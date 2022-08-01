7131d97356fe1529e4419fd2a98bf270b494df68
phoenixewyl987 <phoenixewyl987@gmail.com>
2022-08-01 21:34:14 +0530

    added flitering

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const url = "https://randomuser.me/api/?results=10";
 
 fetch(url)
-    .then(response => {
-      return response.json();
-    })
-    .then(parsedData => {
-     console.log(parsedData);
-     localStorage.setItem("data",JSON.stringify(parsedData.results));
+  .then(response => {
+    return response.json();
   })
-      window.localStorage.getItem("data");
-    let UserDetails = JSON.parse(window.localStorage.getItem("data"))
-    console.log(UserDetails);
-    let details = " ";
-    UserDetails.map(info=>{
-            console.log(info);
-            details += `
+  .then(parsedData => {
+    //console.log(parsedData);
+    localStorage.setItem("data", JSON.stringify(parsedData.results));
+  })
+window.localStorage.getItem("data");
+let UserDetails = JSON.parse(window.localStorage.getItem("data"))
+//console.log(UserDetails);
+let details = " ";
+UserDetails.map(info => {
+  console.log(info);
+  details += `
             <div class="user">
             <p>${info.name.title}.${info.name.first} ${info.name.last}</p>
             <p>${info.dob.age}</p>
@@ -22,6 +22,26 @@ fetch(url)
             <img src=${info.picture.medium}>
            
             </div>`
+})
+document.getElementById("root").innerHTML = details;
+
+const searchname= () =>{
+  charInfo = "";
+   let searchdata =JSON.parse(localStorage.getItem("data"));
+   let val = document.querySelector('.box').value
+   let data = searchdata.filter((item) => item.name.first=== val);
+   data.map ((info) => {
+    details += `
+            <div class="user">
+            <p>${info.name.title}.${info.name.first} ${info.name.last}</p>
+            <p>${info.dob.age}</p>
+            <p>${info.location.country}</p>
+            <img src=${info.picture.medium}>
+           
+            </div>`
+
    })
-   document.getElementById("root").innerHTML = details;
-  
+   console.log(details);
+   document.getElementById("root").innerHTML = details;}
+
+
