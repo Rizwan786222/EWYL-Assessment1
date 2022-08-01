23b76f04ac32989a2a01d5763c718506c6bb1fa4
phoenixewyl987 <phoenixewyl987@gmail.com>
2022-08-01 16:14:04 +0530

    files pushed

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
+const url = "https://randomuser.me/api/?results=10";
 
+fetch(url)
+    .then(response => {
+      return response.json();
+    })
+    .then(parsedData => {
+     console.log(parsedData);
+     localStorage.setItem("data",JSON.stringify(parsedData.results));
+  })
+      window.localStorage.getItem("data");
+    let UserDetails = JSON.parse(window.localStorage.getItem("data"))
+    console.log(UserDetails);
+    let details = " ";
+    UserDetails.map(info=>{
+            console.log(info);
+            details += `
+            <div class="user">
+            <p>${info.name.title}.${info.name.first} ${info.name.last}</p>
+            <p>${info.dob.age}</p>
+            <p>${info.location.country}</p>
+            <img src=${info.picture.medium}>
+           
+            </div>`
+   })
+   document.getElementById("root").innerHTML = details;
   
