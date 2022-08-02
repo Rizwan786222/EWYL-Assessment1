fb72b1fdd1b08dfbb3950852b4cf8837dc4c635c
phoenixewyl987 <phoenixewyl987@gmail.com>
2022-08-03 00:00:34 +0530

    added changes to files

--- script.js
+++ script.js
@@ -6,42 +6,61 @@ fetch(url)
   })
   .then(parsedData => {
     //console.log(parsedData);
-    localStorage.setItem("data", JSON.stringify(parsedData.results));
+   localStorage.setItem("data", JSON.stringify(parsedData.results));
+
+
+    let UserDetails = JSON.parse(localStorage.getItem("data"))
+    //console.log(UserDetails);
+    let details = " ";
+    UserDetails.map(info => {
+      //console.log(info);
+      details += `
+            <div class="user">
+            <p>${info.name.title}.${info.name.first} ${info.name.last}</p>
+            <p>${info.dob.age}</p>
+            <p>${info.location.country}</p>
+            <img src=${info.picture.medium}>
+           
+            </div>`
+    })
+    document.getElementById("root").innerHTML = details;
   })
-window.localStorage.getItem("data");
-let UserDetails = JSON.parse(window.localStorage.getItem("data"))
-//console.log(UserDetails);
-let details = " ";
-UserDetails.map(info => {
-  console.log(info);
-  details += `
-            <div class="user">
-            <p>${info.name.title}.${info.name.first} ${info.name.last}</p>
-            <p>${info.dob.age}</p>
-            <p>${info.location.country}</p>
-            <img src=${info.picture.medium}>
-           
-            </div>`
-})
-document.getElementById("root").innerHTML = details;
-
-const searchname= () =>{
-  charInfo = "";
-   let searchdata =JSON.parse(localStorage.getItem("data"));
-   let val = document.querySelector('.box').value
-   let data = searchdata.filter((item) => item.name.first=== val);
-   data.map ((info) => {
+const searchname = () => {
+  details = "";
+  let searchdata = JSON.parse(localStorage.getItem("data"));
+  let val = document.querySelector('.search-box').value;
+  let userdata = searchdata.filter((item) => item.name.first === val);
+  console.log(userdata);
+  userdata.map(info => {
+    //console.log(info);
     details += `
-            <div class="user">
-            <p>${info.name.title}.${info.name.first} ${info.name.last}</p>
-            <p>${info.dob.age}</p>
-            <p>${info.location.country}</p>
-            <img src=${info.picture.medium}>
-           
-            </div>`
+              <div class="user">
+              <p>${info.name.title}.${info.name.first} ${info.name.last}</p>
+              <p>${info.dob.age}</p>
+              <p>${info.location.country}</p>
+              <img src=${info.picture.medium}>
+             
+              </div>`
+  })
+  document.getElementById("root").innerHTML = details;
 
-   })
-   console.log(details);
-   document.getElementById("root").innerHTML = details;}
+}
 
-
+/*function gotonext(event) { 
+    event.preventDefault(); 
+    window.history.pushState({}, '', event.target.href); 
+    constructPage(); 
+} 
+const routes = { 
+    '/home':'./home.html'  
+} 
+function constructPage() { 
+    let path = window.location.pathname; 
+    fetch("./home.html") 
+    .then(function(htmlContent) { 
+        htmlContent.text() 
+        .then(function(content) { 
+            document.querySelector('.salary').innerHTML = content; 
+        })
+    })
+}
